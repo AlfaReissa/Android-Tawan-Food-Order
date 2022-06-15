dfd23478672a9fb109f37b8a477a585462afb320
manyunyu7 <henryaugusta4@gmail.com>
2022-06-15 09:01:33 +0700

    add base url

--- app/src/main/java/com/tawan/java/utils/Network.kt
+++ app/src/main/java/com/tawan/java/utils/Network.kt
@@ -1,7 +1,6 @@
 package com.tawan.java.utils
 
 object Network {
-//    const val BASE_URL = "http://todolist.feylabs.my.id/api/"
-    const val BASE_URL = "http://192.168.100.4:1680/api/"
+    const val BASE_URL = "http://ta-wan.feylabs.my.id/api/"
     fun getCommonToken() = ""
 }
